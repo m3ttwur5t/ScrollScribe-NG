2717248e48ec2aacbb7ee701bdf73a48343d9ece
Langdon Alger <tsubaki@hotmail.de>
2024-03-24 23:02:46 +0100

    u

--- Build Release.bat
+++ Build Release.bat
@@ -7,8 +7,8 @@ if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build build --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
 
-xcopy "build\release\*.dll" "G:\Skyrim Mod\Mo2-Shiki\mods\Scroll-Scribe-Skyrim-MOD\skse\plugins\" /I /Y
-xcopy "build\release\*.pdb" "G:\Skyrim Mod\Mo2-Shiki\mods\Scroll-Scribe-Skyrim-MOD\skse\plugins\" /I /Y
+xcopy "build\release\*.dll" "dist\SKSE\Plugins\" /I /Y
+xcopy "build\release\*.pdb" "dist\SKSE\Plugins\" /I /Y
 
 xcopy "package" "dist" /I /Y /E
 
